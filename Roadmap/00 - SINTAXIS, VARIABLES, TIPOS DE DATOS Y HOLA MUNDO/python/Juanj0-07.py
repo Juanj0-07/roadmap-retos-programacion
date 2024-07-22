5aa42111bb31da70938e6ed4af158ad0f3705fde
Juan Jose Rios <juanjoserios538@gmail.com>
2024-07-22 08:03:42 -0500

    Create Juanj0-07.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Juanj0-07.py
@@ -0,0 +1,24 @@
+# https://python.org/
+
+# Comentario en una línea
+
+"""
+Comentario en varias líneas
+"""
+
+'''
+Comentario en varias líneas
+'''
+
+# Mis Variables
+name = "Juan R"
+alias = "Juan.Dev"
+
+#Tipos de datos
+date_int = 5 #número entero
+date_float = 5.9 #número decimal
+date_bool = False #
+date_bool = True #
+date_str = "JuanDev" #Cadena de texto 
+
+print ("¡Hola, Python!")
